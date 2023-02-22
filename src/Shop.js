5fd533440bdcd01674bbb6e4f1dbd0c0303e3c84
faraz <shm.faraz@15gmail.com>
2023-02-23 03:32:30 +0530

    added state updation using redux

--- src/Shop.js
+++ src/Shop.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from "./state/index";
 
 export default function Shop() {
+    const dispatch = useDispatch();
+    const { withdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch)
     return (
         <div className='container'>
             <h2>Deposit/Withdraw Money</h2>
-            <button className="btn btn-primary mx-2">+</button>
+            <button className="btn btn-primary mx-2" onClick={() => { depositMoney(100) }}>+</button>
             Update Balance
-            <button className="btn btn-primary mx-2">-</button>
-
+            <button className="btn btn-primary mx-2" onClick={() => { withdrawMoney(100) }}>-</button>
         </div>
     )
 }
